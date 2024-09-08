a29759ccdeb1bda3425c107cb8d9c3a8efc93d0c
andr0meda727 <kwwk747@gmail.com>
2024-09-08 21:51:41 +0200

    02_repeatstring solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
 
+    let repetatedString = '';
+    for (let i = 0; i < num; i++) {
+        repetatedString += string;
+    }
+
+    return repetatedString;
 };
 
 // Do not edit below this line
