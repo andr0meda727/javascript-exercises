3ce3a007174e281fee35d06fdc2c24873a4fed9d
andr0meda727 <kwwk747@gmail.com>
2024-09-08 22:01:31 +0200

    03_reversestring solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedArray = [];
 
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        reversedArray.push(stringArray[i]);
+    }
+    let reversedString = reversedArray.join("");
+
+    return reversedString;
 };
 
 // Do not edit below this line
