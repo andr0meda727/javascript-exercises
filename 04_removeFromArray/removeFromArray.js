ab4228032ea325cce81c5e47b10729bc158f216f
andr0meda727 <kwwk747@gmail.com>
2024-09-09 13:40:03 +0200

    04_removefromarray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const arrayWithoutRemovedValues = []
+    elementsLoop: for (const element of array) {
+        for (const arg of args) {
+            if (element === arg) {
+                continue elementsLoop;
+            }
+        }
+        arrayWithoutRemovedValues.push(element)
+    }
+
+    return arrayWithoutRemovedValues;
 };
 
 // Do not edit below this line
