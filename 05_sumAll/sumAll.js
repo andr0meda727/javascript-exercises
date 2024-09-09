9769d056d21c24dbbaaacf445553e06dbe3f1764
andr0meda727 <kwwk747@gmail.com>
2024-09-09 13:51:04 +0200

    05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (startNum < 0 || endNum < 0) {
+        return 'ERROR'
+    }
 
+    if (!(Number.isInteger(startNum) && Number.isInteger(endNum))) {
+        return "ERROR"
+    }
+
+    let sum = 0;
+    if (endNum > startNum) {
+        for (let i = startNum; i <= endNum; i++) {
+            sum += i;
+        }
+    }
+
+    else {
+        for (let i = startNum; i >= endNum; i--) {
+            sum += i;
+        } 
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
