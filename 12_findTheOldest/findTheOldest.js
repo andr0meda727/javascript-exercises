e11435b853ef8e7edd4fdb9dc9535c7466e59be6
andr0meda727 <kwwk747@gmail.com>
2024-09-13 23:24:37 +0200

    12_findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.map(inputPersonAgeIntoObject)
+    function inputPersonAgeIntoObject(person) {
+        if (person.yearOfDeath) {
+            person["age"] = `${person.yearOfDeath - person.yearOfBirth}`;
+        }
+        else {
+            person["age"] = `${new Date().getFullYear() - person.yearOfBirth}`;
+        }
+    }
 
+    let greatestAge = 0;
+    for (let person of people) {
+        if (Number(person.age) > Number(greatestAge)) {
+            greatestAge = person.age;
+        }
+    }
+    
+
+    return people.find(person => person.age == greatestAge);
 };
 
 // Do not edit below this line
