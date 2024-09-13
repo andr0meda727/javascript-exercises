8facbbbe1f8a71fd0f957e2b52a0573790aaf900
andr0meda727 <kwwk747@gmail.com>
2024-09-13 22:17:12 +0200

    09_palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    let array = string.split('');
+    let arrayReversed = array.slice();
+    arrayReversed.reverse();
+    return array.every((value, index) => value == arrayReversed[index])
 };
 
 // Do not edit below this line
